1c1d55abbf60330d20ee332756e2caa8da3876e7
Arthur <zenaboa7@gmail.com>
2023-09-01 11:08:54 -0300

    TCC

--- salao/server/app.js
+++ salao/server/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const corsOptions = {
-    origin: 'http://177.91.120.132',
-    optionsSuccessStatus: 200,
-  };
+// const corsOptions = {
+//     origin: 'http://177.91.120.132',
+//     optionsSuccessStatus: 200,
+//   };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(express.json());
 
 module.exports = { app };
